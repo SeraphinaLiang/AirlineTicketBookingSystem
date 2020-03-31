1f4c44cb9583066d69ee0e459a135bf9f90dd6ee
SeraphinaLiang <1010814876@qq.com>
2020-03-31 17:13:01 +0800

    界面finish

--- AirlineTicketBookingSystem/src/screen/managePageController.java
+++ AirlineTicketBookingSystem/src/screen/managePageController.java
@@ -38,28 +38,38 @@ public class managePageController implements Initializable {
 
 	@FXML
 	void managerLogin(ActionEvent event) {
-		String account = null, key = null;
+//最后改回登录
+//		String account = null, key = null;
+//
+//		if (!this.tfAccount.getText().isEmpty()) {
+//			account = this.tfAccount.getText();
+//		}
+//		if (!this.tfPassword.getText().isEmpty()) {
+//			key = this.tfPassword.getText();
+//		}
+//		if ((account != null) && (key != null)) {
+//			if (account.equals(main.Main.managerAccount) && key.equals(main.Main.managerPassword)) {
+//				  
+//				try {
+//					Pane pane = FXMLLoader.load(getClass().getResource("managePage.fxml"));
+//					this.basic.getChildren().setAll(pane);
+//
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		}
 
-		if (!this.tfAccount.getText().isEmpty()) {
-			account = this.tfAccount.getText();
-		}
-		if (!this.tfPassword.getText().isEmpty()) {
-			key = this.tfPassword.getText();
-		}
-		if ((account != null) && (key != null)) {
-			if (account.equals(main.Main.managerAccount) && key.equals(main.Main.managerPassword)) {
-				  
-				try {
-					Pane pane = FXMLLoader.load(getClass().getResource("managePage.fxml"));
-					this.basic.getChildren().setAll(pane);
+		
+		try {
+			Pane pane = FXMLLoader.load(getClass().getResource("managePage.fxml"));
+			this.basic.getChildren().setAll(pane);
 
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
 	}
 
 }
